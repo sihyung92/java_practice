e3e028d8850ce5f0b5d71d5dd301edd32184c7a3
Sung Si Hyung <fjzjqhdl@gmail.com>
2020-03-10 17:46:02 +0900

    feat : 시간 관련 예제 풀이
    - 요일구하기
    - 태어난 날로부터 며칠?
    - n째주 n요일 날짜 구하기

--- src/main/java/com/practice/util/CalendarClassExercise.java
+++ src/main/java/com/practice/util/CalendarClassExercise.java
@@ -2,7 +2,14 @@ package com.practice.util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class CalendarClassExercise {
     public static void main(String[] args) {
@@ -11,6 +18,15 @@ public class CalendarClassExercise {
         afterEachExercise();
         beforeEachExercise("exercise2", "두 날짜사이의 월급일 구하기");
         exercise2("2010-01-20", "2010-01-21");
+        afterEachExercise();
+        beforeEachExercise("exercise3", "요일 구하기");
+        exercise3();
+        afterEachExercise();
+        beforeEachExercise("exercise4", "태어난 날로부터 며칠");
+        exercise4(1992, 06, 01);
+        afterEachExercise();
+        beforeEachExercise("exercise5", "n째주 n요일 날짜구하기");
+        exercise5();
         afterEachExercise();
     }
 
@@ -71,5 +87,61 @@ public class CalendarClassExercise {
         } catch (ParseException e) {
             System.out.println("날짜 형식은 yyyy-MM-dd입니다");
         }
+    }
+
+    private static void exercise3() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (Objects.isNull(input) || input.isEmpty()) {
+                System.out.println("아무것도 입력 안 하셨는뎁쇼");
+                continue;
+            }
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+            try {
+                Date date = simpleDateFormat.parse(input);
+                simpleDateFormat.applyPattern("입력하신 날짜는 E요일입니다.");
+                System.out.println(simpleDateFormat.format(date));
+            } catch (ParseException e) {
+                System.out.println("양식에 맞게 입력해주세요");
+                continue;
+            }
+            break;
+        }
+    }
+
+    /*
+     * 태어난 날로부터 며칠 지났는지 구하기
+     * */
+    private static void exercise4(int year, int month, int date) {
+//        Calendar now = Calendar.getInstance();
+//        Calendar birthDay = Calendar.getInstance();
+//        birthDay.set(year, month - 1, date);
+//        Long dayOfNow = now.getTimeInMillis() / (1000 * 60 * 60 * 24);
+//        Long dayOfBirth = birthDay.getTimeInMillis() / (1000 * 60 * 60 * 24);
+//        Long dayFromBirth = dayOfNow - dayOfBirth;
+//        if (dayFromBirth < 0) {
+//            dayFromBirth = 0L;
+//        }
+//        System.out.printf("태어난 날로부터 %d일입니다.\n", dayFromBirth);
+        LocalDate birthDay = LocalDate.of(year, month, date);
+        long until = birthDay.until(LocalDate.now(), ChronoUnit.DAYS);
+        System.out.printf("태어난 날로부터 %d일입니다.\n", until);
+    }
+
+    /*
+     * 2016년 12월 넷째주 화요일의 날짜를 아래의 실행결과와 같은 형식으로 출력하시오
+     * ex ) 2016-12-27
+     */
+    private static void exercise5() {
+//        Calendar cal = Calendar.getInstance();
+//        cal.set(Calendar.YEAR, 2020);
+//        cal.set(Calendar.MONTH, 2);
+//        cal.set(Calendar.WEEK_OF_MONTH, 4);
+//        cal.set(Calendar.DAY_OF_WEEK, 1);
+//        System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime()));
+        LocalDate date = LocalDate.of(2016, 12, 1);
+        LocalDate resultDate = date.with(TemporalAdjusters.dayOfWeekInMonth(4, DayOfWeek.TUESDAY));
+        System.out.println(resultDate);
     }
 }
